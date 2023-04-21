1bd0bad3f0b64f5f43fda8ef2e6d42f4cbec9a1b
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-04-20 22:01:29 -0400

    Update all traveler test, class and data

--- test/Traveler-Test.js
+++ test/Traveler-Test.js
@@ -1,15 +1,15 @@
 import chai from 'chai';
-const expect = chai.expect;
-import { testTravelerData } from '../src/data/traveler-data';
+import { travelerData } from '../src/data/traveler-data';
 import Traveler from '../src/Traveler';
 
+const expect = chai.expect;
 
 describe('Traveler', () => {
   let traveler;
   let travelData;
 
   beforeEach(() => {
-    travelData = testTravelerData;
+    travelData = travelerData;
     traveler = new Traveler(travelData[0]);
   });
 
@@ -21,10 +21,9 @@ describe('Traveler', () => {
     expect(traveler).to.be.an.instanceOf(Traveler);
   });
 
-  it('should store the traveler\'s data', () => {
+  it('should store the traveler data', () => {
     expect(traveler.id).to.equal(1);
     expect(traveler.name).to.equal('Ham Leadbeater');
     expect(traveler.travelerType).to.equal('relaxer');
   });
-
 });
