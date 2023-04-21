8940ee0f2408bfa7e4d085854330ab4eeef24c54
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-04-21 11:41:37 -0400

    Update testing and classes for Trips

--- src/Trip.js
+++ src/Trip.js
@@ -1,6 +1,3 @@
-import Destination from "./Destination";
-import { tripData } from "./data/trip-data";
-
 class Trip {
   constructor(tripData) {
     this.id = tripData.id;
@@ -11,38 +8,7 @@ class Trip {
     this.duration = tripData.duration;
     this.status = tripData.status;
     this.suggestedActivities = tripData.suggestedActivities;
-    this.trips = [];
   }
-
-  tripDataLoad(tripData) {
-    this.trips = tripData.map((tripInfo) => { return new Trip(tripInfo) });
-  }
-  
-    tripsByTraveler(traveler) {
-      const tripRows = this.trips.reduce((arr, trip) => {
-        if (trip.userID === traveler.id) {
-          arr.push(trip);
-        }
-        console.log(`trip.userID: ${trip.userID}, traveler.id: ${traveler.id}`);
-        return arr;
-      }, [])
-      return tripRows
-    }
-    yearlyCost(destination, traveler) {
-      const travelerTrips = this.tripsByTraveler(traveler);
-      const tripsByYear = travelerTrips.filter(trip => trip.date.includes('2022') && trip.status === "approved");
-      const totalCostOfTripsAnnually = tripsByYear.reduce((num, trip) => {
-        num += this.costPerTrip(destination);
-        return num;
-      }, 0);
-      return Math.round(totalCostOfTripsAnnually);
-    }
-
-    costPerTrip(destination) {
-      const totalLodgingCost = destination.lodgingCost * this.duration * this.travelers;
-      const totalFlightCost = destination.flightCost * this.travelers;
-      return Math.round((totalLodgingCost + totalFlightCost) * 1.1);
-    }
 }
 
 export default Trip;
--- test/Trip-Test.js
+++ test/Trip-Test.js
@@ -4,6 +4,7 @@ import { tripData } from '../src/data/trip-data';
 import { destinationData } from '../src/data/destination-data';
 import { travelerData } from '../src/data/traveler-data'
 import Trip from '../src/Trip';
+import TripManager from '../src/TripManager';
 import Traveler from '../src/Traveler';
 import Destination from '../src/Destination';
 
@@ -16,7 +17,6 @@ describe('Trip', function() {
     destination = new Destination(destinationData[0]);
     trip = new Trip(tripData[0]);
     traveler = new Traveler(travelerData[0]);
-    trip.tripDataLoad(tripData)
   });
 
   it("should be a function", function () {
@@ -37,13 +37,35 @@ describe('Trip', function() {
     expect(trip.status).to.equal('approved');
     expect(trip.suggestedActivities).to.deep.equal([]);
   });
+});
 
-  it("should calculate the total cost of a trip", function () {
-    expect(trip.costPerTrip(destination)).to.equal();
+  describe('TripManager', function() {
+    let destination;
+    let tripManager; 
+    let traveler;
+
+  beforeEach(() => {
+    destination = new Destination(destinationData[0]);
+    tripManager = new TripManager;
+    traveler = new Traveler(travelerData[0]);
+    tripManager.loadTripInfo(tripData);
   });
 
+  it("should be a function", function () {
+    expect(TripManager).to.be.a("function");
+  });
+
+  it("should be an instances of trip", function () {
+    expect(tripManager).to.be.an.instanceof(TripManager)
+  });
+
+  // it("should calculate the total cost of a trip", function () {
+  //   const trip = tripManager.trips[0];
+  //   expect(tripManager.costPerTrip(trip, destination)).to.equal(0);
+  // });
+
   it("should find trips by traveler ID", function () {
-    expect(trip.tripsByTraveler(traveler)).to.deep.equal([
+    expect(tripManager.tripsByTraveler(traveler)).to.deep.equal([
          {
           "id": 1,
           "userID": 44,
@@ -57,8 +79,10 @@ describe('Trip', function() {
       ]);
   });
 
-  it("should calculate the total cost of annual trips for a user", function () {
-    expect(trip.yearlyCost(destination, traveler)).to.equal();
-  });
+  // it("should calculate the total cost of annual trips for a user", function () {
+  //   const year = '2022';
+  //   const expectedAnnualCost = 0; 
+  //   expect(tripManager.yearlyCost(destination, traveler, year)).to.equal(expectedAnnualCost);
+  // });
 
 })
