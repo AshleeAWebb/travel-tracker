31512e34063bb84260af9e894d4fecbe8a3f159e
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-04-25 06:59:02 -0400

    Update ES6

--- test/DataHandler-Test.js
+++ test/DataHandler-Test.js
@@ -1,42 +1,43 @@
 import chai from 'chai';
-const expect = chai.expect;
 import DataHandler from '../src/DataHandler';
 import testData from '../src/data/testData';
 
-describe('Data HandDataHandler', function() {
+const expect = chai.expect;
+
+describe('Data HandDataHandler',() => {
   let dataHandler;
 
-  beforeEach(function() {
+  beforeEach(() => {
     dataHandler = new DataHandler;
   });
 
-  it('should be a function', function() {
+  it('should be a function',() => {
     expect(DataHandler).to.be.a('function');
   });
 
-  it('should be an instance of data handler', function() {
+  it('should be an instance of data handler',() => {
     expect(dataHandler).to.be.instanceOf(DataHandler);
   });
 
-  it('should start with no trip data', function() {
+  it('should start with no trip data',() => {
     expect(dataHandler.allTrips).to.be.null;
   });
 
-  it('should set data into the correct place', function() {
+  it('should set data into the correct place',() => {
     expect(dataHandler.allTrips).to.be.null;
     dataHandler.setData('allTrips', testData.testTripData);
     expect(dataHandler.allTrips).to.deep.equal(testData.testTripData);
   });
 
 
-  it('should return specific data by ID', function() {
+  it('should return specific data by ID',() => {
     dataHandler.setData('allTrips', testData.testTripData);
     expect(dataHandler.getDataByID('allTrips', 'id', 1)).to.deep.equal([testData.testTripData[0]]);
     expect(dataHandler.getDataByID('allTrips', 'userID', 1)).to.deep.equal([testData.testTripData[0], testData.testTripData[2], testData.testTripData[4]]);
     expect(dataHandler.getDataByID('allTrips', 'destinationID', 1)).to.deep.equal([testData.testTripData[0]]);
   });
   
-  it('should return a list of all destination names', function() {
+  it('should return a list of all destination names',() => {
     dataHandler.setData('destinations', testData.testDestinationData);
     expect(dataHandler.getDestinationInfo()).to.deep.equal([{
       name: 'Lima, Peru',
