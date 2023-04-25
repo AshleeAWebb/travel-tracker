1367a81f1e68761914c9a189aabd55dc4abf2b1e
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-04-25 07:35:22 -0400

    reorgainze donut chart for readability

--- src/charts.js
+++ src/charts.js
@@ -1,38 +1,46 @@
 import { Chart } from 'chart.js/auto';
 
-export const generateDonutChart = (traveler) => {
-  const totalSpent = traveler.getYearlySpent();
+const getChartData = (totalSpent) => {
   const amountSpent = totalSpent / 1.1;
   const commission = totalSpent - amountSpent;
+  return {
+    labels: ['Amount Spent', 'Travel Agent Fee'],
+    datasets: [{
+      label: 'Amount',
+      data: [amountSpent, commission],
+      backgroundColor: ['#D68C7A', '#544948']
+    }]
+  };
+};
+
+const getChartOptions = () => {
+  return {
+    plugins: {
+      legend: {
+        labels: {
+          font: {
+            size: 20
+          }
+        }
+      }
+    },
+    title: {
+      display: true,
+      text: 'Total Amount Spent This Year (including 10% travel agent fee)',
+      fontSize: 24 
+    }
+  };
+};
+
+export const generateDonutChart = (traveler) => {
+  const totalSpent = traveler.getYearlySpent();
 
   const donutChartCanvas = document.getElementById('donutChartCanvas');
 
   const donutChart = new Chart(donutChartCanvas, {
     type: 'doughnut',
-    data: {
-      labels: ['Amount Spent', 'Travel Agent Fee'],
-      datasets: [{
-        label: 'Amount',
-        data: [amountSpent, commission],
-        backgroundColor: ['#D68C7A', '#544948']
-      }]
-    },
-    options: {
-      plugins: {
-        legend: {
-          labels: {
-            font: {
-              size: 20
-            }
-          }
-        }
-      },
-      title: {
-        display: true,
-        text: 'Total Amount Spent This Year (including 10% travel agent fee)',
-        fontSize: 24 
-      },
-    }
+    data: getChartData(totalSpent),
+    options: getChartOptions()
   });
 
   const totalSpentDisplay = document.getElementById('totalSpent');
